f7b9b54b6e839cd364f623b645cb1a17bfbaf855
karl.chan <beng_06@yahoo.com>
2016-01-29 14:19:58 +0800

    Code Cleanup

--- app/Providers/FileServiceProvider.php
+++ app/Providers/FileServiceProvider.php
@@ -8,8 +8,6 @@ use App\Services\Contracts\FileService;
 
 class FileServiceProvider extends ServiceProvider
 {
-    protected $defer = true;
-	
     /**
      * Bootstrap the application services.
      *
@@ -27,27 +25,6 @@ class FileServiceProvider extends ServiceProvider
      */
     public function register()
     {
-//         $this->app->singleton('App\Services\Contracts\FileService', function () {
-//             return new FileServiceImpl();
-//         });
-
-//     	$this->app->bind('App\Services\Contracts\FileService', function(){
-    	
-//     		return new FileServiceImpl();
-    	
-//     	});
-
     	$this->app->bind('App\Services\Contracts\FileService', 'App\Services\Impl\FileServiceImpl');
     }
-    
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-    	return ['App\Services\Contracts\FileService'];
     }
-    
-}
